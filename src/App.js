ec75649d1175d9ee2fa1aab9a11f313a1849db43
Somisetty Srinivas Prudhvi <prudhvi.somisetty@gmail.com>
2021-07-27 18:50:27 +0530

    V1.1

--- src/App.js
+++ src/App.js
@@ -68,37 +68,41 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <div>
-          <Webcam
-            ref={webcamRef}
-            style={{
-              position: "absolute",
-              marginLeft: "auto",
-              marginRight: "auto",
-              left: "0",
-              right: "0",
-              textAlign: "center",
-              zIndex: 9,
-              width: 640,
-              height: 480,
-            }}
-          />
-          <canvas
-            ref={canvasRef}
-            style={{
-              position: "absolute",
-              marginLeft: "auto",
-              marginRight: "auto",
-              left: "0",
-              right: "0",
-              textAlign: "center",
-              zIndex: 9,
-              width: 640,
-              height: 480,
-            }}
-          />
-        </div>
+        <p>Image Segementation using BodyPix Model</p>
       </header>
+      <div>
+        <Webcam
+          ref={webcamRef}
+          style={{
+            position: "absolute",
+            marginLeft: "auto",
+            marginRight: "auto",
+            left: "0",
+            right: "0",
+            textAlign: "center",
+            zIndex: 9,
+            width: 640,
+            height: 480,
+          }}
+        />
+        <canvas
+          ref={canvasRef}
+          style={{
+            position: "absolute",
+            marginLeft: "auto",
+            marginRight: "auto",
+            left: "0",
+            right: "0",
+            textAlign: "center",
+            zIndex: 9,
+            width: 640,
+            height: 480,
+          }}
+        />
+      </div>
+      <div class="footer">
+        <p>Developed by: Srinivas Prudhvi, Akshata, Asish</p>
+      </div>
     </div>
   );
 }
